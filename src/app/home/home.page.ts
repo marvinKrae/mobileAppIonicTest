417dc6140582d20805318d3ce4e1c7171ed0290b
Marvin Kraemer <49152971+marvinKrae@users.noreply.github.com>
2020-12-22 10:22:52 +0100

    initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Store } from '../interfaces/store';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  foodLocations: Store[] = [
+    {
+      "name": "Five Guys"
+    },
+    {
+      "name": "Carl's Brauhaus"
+    },
+    {
+      "name": "Netto"
+    },
+    {
+      "name": "McDonald's"
+    },
+    {
+      "name": "Hans im Glück"
+    },
+    {
+      "name": "Kübler"
+    },
+    {
+      "name": "Fritty Bar"
+    },
+    {
+      "name": "LIDL"
+    },
+    {
+      "name": "ALDI"
+    }
+  ]
   constructor() {}
 
 }
